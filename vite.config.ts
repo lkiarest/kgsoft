3c2cd7362db1c5bc9ac69c251df58cd9ef4ff12c
qintx <rushi_wowen@163.com>
2024-09-15 00:22:46 +0800

    fix: remove prerender

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,24 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import wasm from 'vite-plugin-wasm';
 import preact from '@preact/preset-vite';
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		wasm(),
 		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-				additionalPrerenderRoutes: ['/404'],
-				previewMiddlewareEnabled: true,
-				previewMiddlewareFallback: '/404',
-			},
+			// prerender: {
+			// 	enabled: true,
+			// 	renderTarget: '#app',
+			// 	additionalPrerenderRoutes: ['/idcard', '/about', '/404'],
+			// 	previewMiddlewareEnabled: true,
+			// 	previewMiddlewareFallback: '/404',
+			// },
+		}),
+		topLevelAwait({
+			promiseExportName: '__tla',
+			promiseImportName: (i) => `__tla_${i}`,
 		}),
 	]
 });
